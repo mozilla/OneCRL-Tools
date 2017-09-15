2221813014443ae82ed017c5ccb3f4674316dc5f
Mark Goodwin <mgoodwin@mozilla.com>
2017-09-15 10:25:22 +0100

    Add support for retrieving revoked cert info from an arbitrary location

--- salesforce/salesforce.go
+++ salesforce/salesforce.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"net/http"
 	"strings"
 )
 
@@ -22,6 +23,8 @@ type RevokedCertInfo struct {
 	CRLs		  []string
 	CSN			  string
 	CertName	  string
+	SerialNumber  string
+	Reason		  string
 }
 
 
@@ -51,9 +54,41 @@ func FetchSalesforceCSV(stream io.ReadCloser) SalesforceCSV {
 	return records
 }
 
+func FetchRevokedCertInfoFrom(location string) ([]RevokedCertInfo, error) {
+	var stream io.ReadCloser
+
+	if 0 == strings.Index(strings.ToUpper(location), "HTTP") {
+		if 0 != strings.Index(strings.ToUpper(location), "HTTPS") {
+			// cowardly refuse to get cert info from a non-https URL
+			return make([]RevokedCertInfo, 0), errors.New("Cowardly refusing to load data from a non-HTTPS URL")
+		}
+		fmt.Printf("loading salesforce data from %s\n", location)
+
+		// get the stream from URL
+		r, err := http.Get(location)
+		if err != nil {
+			return make([]RevokedCertInfo, 0), errors.New(fmt.Sprintf("problem fetching salesforce data from URL %s\n", err))
+		}
+		defer r.Body.Close()
+
+		stream = r.Body
+	} else {
+		fmt.Printf("loading salesforce data from %s\n", location)
+		// get the stream from a file
+		csvfile, err := os.Open(location)
+		if err != nil {
+			return make([]RevokedCertInfo, 0), errors.New(fmt.Sprintf("problem loading salesforce data from file %s\n", err))
+		}
+
+		stream = io.ReadCloser(csvfile)
+	}
+
+	return FetchRevokedCertInfo(stream), nil
+}
+
 func FetchRevokedCertInfo(stream io.ReadCloser) []RevokedCertInfo {
 	records := FetchSalesforceCSV(stream)
-	revoked := make([]RevokedCertInfo, len(records.Rows))
+	certs := make([]RevokedCertInfo, len(records.Rows))
 
 	for _, each := range records.Rows {
 		certInfo := RevokedCertInfo{}
@@ -61,15 +96,20 @@ func FetchRevokedCertInfo(stream io.ReadCloser) []RevokedCertInfo {
 		certInfo.PEM = each[records.ColumnNames["PEM Info"]]
 		certInfo.AlternateCRL = each[records.ColumnNames["Alternate CRL"]]
 		certInfo.CRLs = strings.Split(each[records.ColumnNames["CRL URL(s)"]] + ", " + certInfo.AlternateCRL, ", ")
+		certInfo.Reason= each[records.ColumnNames["RFC 5280 Revocation Reason Code"]]
 
 		// Also get some data for more helpful errors
 		certInfo.CSN = each[records.ColumnNames["Certificate Serial Number"]]
 		certInfo.CertName = each[records.ColumnNames["CA Owner/Certificate Name"]]
 
-		revoked = append(revoked, certInfo)
+		// And get some data for reconciling OneCRL entries and CCADB entries
+		// for reporting
+		certInfo.SerialNumber= each[records.ColumnNames["Certificate Serial Number"]]
+
+		certs = append(certs, certInfo)
 	}
 
-	return revoked
+	return certs
 }
 
 func CertDataFromSalesforcePEM (PEM string) ([]byte, error) {
