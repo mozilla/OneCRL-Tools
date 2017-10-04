bea1036f888e167cc13768ba00418dc80a2e59f1
Mark Goodwin <mgoodwin@mozilla.com>
2017-10-04 19:06:43 +0100

    Issue #88 - ensure exceptions via HTTPS are in the Lambda command args

--- tools/Salesforce2OneCRL-scheduler/scheduler.py
+++ tools/Salesforce2OneCRL-scheduler/scheduler.py
@@ -9,7 +9,7 @@ def ccadb_import(event, context):
         lambda_task_root = os.environ['LAMBDA_TASK_ROOT']
     command = 'PATH="%s" LAMBDA_TASK_ROOT="%s" %s/salesforce2OneCRL' % (os.environ['PATH'], lambda_task_root, lambda_task_root)
     print("Calling CCADB import client using: %s" % (command))
-    exit_code = subprocess.call([lambda_task_root + '/salesforce2OneCRL'], env=os.environ.copy())
+    exit_code = subprocess.call([lambda_task_root + '/salesforce2OneCRL', '--exceptions', 'https://raw.githubusercontent.com/mozilla/OneCRL-Tools/master/salesforce2OneCRL/data/exceptions.json'], env=os.environ.copy())
     if exit_code > 0:
         raise ValueError("CCADB import failed! Check the logs for more information.")
 
