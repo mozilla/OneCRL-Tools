e03ae3bf3a5e813f47fc899975047aa4ab44c350
Mark Goodwin <mgoodwin@mozilla.com>
2017-10-06 12:09:31 +0100

    Issue #90- move args into a separate array, URL into constant for more readable code

--- tools/Salesforce2OneCRL-scheduler/scheduler.py
+++ tools/Salesforce2OneCRL-scheduler/scheduler.py
@@ -3,13 +3,22 @@
 import os, sys
 import subprocess
 
+
+EXCEPTIONS_URL = 'https://raw.githubusercontent.com/mozilla/OneCRL-Tools/master/salesforce2OneCRL/data/exceptions.json'
+
 def ccadb_import(event, context):
     lambda_task_root = "."
+    binary_args = [
+            '--exceptions',
+            EXCEPTIONS_URL
+            ]
     if 'LAMBDA_TASK_ROOT' in os.environ:
         lambda_task_root = os.environ['LAMBDA_TASK_ROOT']
-    command = 'PATH="%s" LAMBDA_TASK_ROOT="%s" %s/salesforce2OneCRL' % (os.environ['PATH'], lambda_task_root, lambda_task_root)
-    print("Calling CCADB import client using: %s" % (command))
-    exit_code = subprocess.call([lambda_task_root + '/salesforce2OneCRL', '--exceptions', 'https://raw.githubusercontent.com/mozilla/OneCRL-Tools/master/salesforce2OneCRL/data/exceptions.json'], env=os.environ.copy())
+    variables = 'PATH="%s" LAMBDA_TASK_ROOT="%s"' % (os.environ['PATH'], lambda_task_root)
+    subprocess_call_args = [lambda_task_root + '/salesforce2OneCRL']
+    subprocess_call_args.extend(binary_args)
+    print("Calling CCADB import client using: %s and args %s" % (variables, " ".join(subprocess_call_args)))
+    exit_code = subprocess.call(subprocess_call_args, env=os.environ.copy())
     if exit_code > 0:
         raise ValueError("CCADB import failed! Check the logs for more information.")
 
