341c0d2f5e67a24a87cfc4910eef11099f1a88a0
J.C. Jones <jc@mozilla.com>
2020-09-18 22:51:32 -0700

    Ensure version.json is in the containers

--- containers/build-local.sh
+++ containers/build-local.sh
@@ -3,4 +3,11 @@
 VER=staging
 
 cd $(dirname ${0})
+printf '{"commit":"%s","version":"%s","source":"https://github.com/%s/%s","build":"%s"}\n' \
+                "$(git rev-parse HEAD)" \
+                "$(git describe --tags)" \
+                "$(id -u -n)" \
+                "OneCRL-Tools" \
+                "localhost" > ../version.json
 docker build -t ccadb2onecrl:${VER} .. -f Dockerfile
+rm ../version.json
