960a129691a6ff9f0e02202091435a380aae0e2c
J.C. Jones <jc@mozilla.com>
2020-11-10 15:48:45 -0700

    Add tests of the Base64 decoder

--- ccadb2OneCRL/utils/utils_test.go
+++ ccadb2OneCRL/utils/utils_test.go
@@ -4,4 +4,32 @@
 
 package utils
 
-import ()
+import (
+	"bytes"
+	"testing"
+)
+
+func assertBytes(t *testing.T, data string, expected []byte) {
+	result, err := B64Decode(data)
+	if err != nil {
+		t.Errorf("Expected %s to decode to %v but error: %v", data, expected, err)
+		return
+	}
+	if !bytes.Equal(result, expected) {
+		t.Errorf("Expected %s to decode to %v but got: %v", data, expected, result)
+	}
+}
+
+func TestBase64Decoder(t *testing.T) {
+	// Tolerate spaces
+	assertBytes(t, "b2theSB0aGVyZQ==", []byte("okay there"))
+	assertBytes(t, "b2theSB0aGVyZQ== ", []byte("okay there"))
+	assertBytes(t, " b2theSB0aGVyZQ==", []byte("okay there"))
+	assertBytes(t, " b2theSB0aGVyZQ== ", []byte("okay there"))
+
+	// Handle high bytes
+	assertBytes(t, "/+7dzLuqmYg=", []byte{0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88})
+
+	// Handle having no padding
+	assertBytes(t, "/+7dzLuqmYg", []byte{0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88})
+}
